70c9270bb325a82b8ceeb63cc3b0c47e0898cf84
Yohanne Karine <yohannekarinek@gmail.com>
2026-06-18 11:08:28 -0300

    alteracao

---
+++ aula 12.js/aula-6.js
@@ -0,0 +1,4 @@
+//Desenvolver um programa que leia um número n qualquer menor ou igual a 50 e apresente o valor obtido da
+//multiplicação sucessiva de n por 3 enquanto o produto for menor que 250. (n x 3; n x 3 x 3; n x 3 x 3 x 3 etc...).
+
+let cont = 15;
